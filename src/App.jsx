aa58f8730132bc8b44095391e508be26d568bf95
Amal Raj M P <amalrajmp72@gmail.com>
2026-06-25 00:05:10 +0530

    chore: initialize project structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Super App</h1>
-}
+import { Routes, Route } from "react-router-dom"
+
+import Register from "./pages/Register"
+import Categories from "./pages/Categories"
+import Dashboard from "./pages/Dashboard"
+import Movies from "./pages/Movies"
+import NotFound from "./pages/NotFound"
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Register />} />
+    <Route path="/categories" element={<Categories />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/movies" element={<Movies />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+)
 
 export default App
